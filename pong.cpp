f29de3258cdc41434d693e90fb58b69b2ff11a95
David Frey <honggoff@gmx.ch>
2024-10-13 22:34:57 +0200

    Next iteration, now with ball bouncing off walls and paddles

--- pong.cpp
+++ pong.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <cstdio>
+#include <chrono>
 #include <unistd.h>
 #include <string>
 #include <random>
@@ -9,8 +10,14 @@ extern "C"
 #include <tfblib/tfb_colors.h>
 }
 
+using namespace std::chrono_literals;
+
 const auto& BACKGROUND = tfb_black;
 const auto& FOREGROUND = tfb_white;
+const auto INTERVAL = 100ms;
+const auto POINTS = 5;
+const auto SPEED_FACTOR = 0.02f;
+
 
 class PongField
 {
@@ -34,10 +41,41 @@ public:
         drawBall();
     }
 
-    void moveBall(int dx, int dy)
+    void moveBall(int& dx, int& dy)
     {
-        mBallX += dx;
-        mBallY += dy;
+        auto newX = mBallX + dx;
+        auto newY = mBallY + dy;
+        // upper wall
+        if (newY < 0)
+        {
+            dy = -dy;
+            newY = - newY;
+        }
+
+        // lower wall
+        if (newY > mHeight)
+        {
+            dy = -dy;
+            newY = 2 * mHeight - newY;
+        }
+
+        // left paddle
+        const auto paddle1Pos = 2 * mPaddleWidth;
+        if (mBallX > paddle1Pos and newX <= paddle1Pos and 2 * std::abs(newY - mPlayer1) <= mPaddleHeight)
+        {
+            dx = -dx;
+            newX = (2 * paddle1Pos) - newX;
+        }
+        // right paddle
+        const auto paddle2Pos = mWidth - 2 * mPaddleWidth;
+        if (mBallX < paddle2Pos and newX >= paddle2Pos and 2 * std::abs(newY - mPlayer2) <= mPaddleHeight)
+        {
+            dx = -dx;
+            newX = (2 * paddle2Pos) - newX;
+        }
+
+        mBallX = newX;
+        mBallY = newY;
     }
 
     void move1(int dist)
@@ -60,13 +98,30 @@ public:
         mScore2++;
     }
 
-    bool wouldScore(int dx, int dy)
+    int score(int dx, int dy)
     {
+        const auto paddle1Pos = 2 * mPaddleWidth;
+        const auto paddle2Pos = mWidth - 2 * mPaddleWidth;
+
         const auto newX = mBallX + dx;
-        if (newX <= 0 or newX >= mWidth)
-            return true;
-        return false;
+        if (mBallX < paddle1Pos and newX <= 0)
+            return 2;
+        if (mBallX > paddle2Pos and newX >= mWidth)
+            return 1;
+        return 0;
     }
+
+    bool gameOver()
+    {
+        return (mScore1>= POINTS or mScore2 >= POINTS);
+    }
+
+    void resetBall()
+    {
+        mBallX = mWidth / 2;
+        mBallY = mHeight / 2;;
+    }
+
 private:
 
     void drawPaddle(size_t x, size_t y)
@@ -97,19 +152,13 @@ private:
         tfb_fill_rect(mBallX - mBallSize / 2, mBallY - mBallSize / 2, mBallSize, mBallSize, FOREGROUND);
     }
 
-    void resetBall()
-    {
-        mBallX = mWidth / 2;
-        mBallY = mHeight / 2;;
-    }
-
     const size_t mWidth;
     const size_t mHeight;
     const size_t mPaddleHeight;
     const size_t mPaddleWidth;
     const size_t mBallSize;
-    size_t mPlayer1;
-    size_t mPlayer2;
+    int mPlayer1;
+    int mPlayer2;
     int mScore1;
     int mScore2;
     int mBallX;
@@ -121,7 +170,7 @@ class PongGame
 public:
     PongGame(size_t width, size_t height) :
         mField(width, height),
-        mSpeed(width * 0.01f),
+        mSpeed(width * SPEED_FACTOR),
         mRand(std::random_device()())
     { }
 
@@ -129,31 +178,48 @@ public:
     {
         resetSpeed();
 
-        tfb_clear_screen(BACKGROUND);
-        mField.draw();
-        tfb_flush_window();
-
-        sleep(1);
         mField.move1(10);
         mField.move2(-20);
         mField.score1();
         mField.score2();
         mField.score2();
 
-        while (not mField.wouldScore(mSpeedx, mSpeedy))
+        while (not mField.gameOver())
         {
+            const auto lastExecution = std::chrono::high_resolution_clock::now();
             tick();
             tfb_clear_screen(BACKGROUND);
             mField.draw();
             tfb_flush_window();
+
+            const auto now = std::chrono::high_resolution_clock::now();
+            const auto next = lastExecution + INTERVAL;
+            const auto sleepTime = next - now;
+            const auto sleepUs = std::chrono::duration_cast<std::chrono::microseconds>(sleepTime).count();
+
+            if (sleepUs > 0)
+                usleep(sleepUs);
+            else
+                printf("delayed frame (%ldus)\n", -sleepUs);
         }
-
-
     }
 
 private:
     void tick()
     {
+        switch (mField.score(mSpeedx, mSpeedy))
+        {
+            case 1:
+                mField.score1();
+                resetSpeed();
+                mField.resetBall();
+                break;
+            case 2:
+                mField.score2();
+                resetSpeed();
+                mField.resetBall();
+                break;
+        }
         mField.moveBall(mSpeedx, mSpeedy);
     }
 
