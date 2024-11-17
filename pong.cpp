62e47bc5e1f2168304c09b7404d02b784d07c00b
David Frey <honggoff@gmx.ch>
2024-11-17 15:45:56 +0100

    Add start of event implementation

--- pong.cpp
+++ pong.cpp
@@ -1,8 +1,13 @@
 #include <cstdio>
 #include <chrono>
-#include <unistd.h>
-#include <string>
+#include <fcntl.h>
+#include <libevdev/libevdev.h>
+#include <map>
 #include <random>
+#include <queue>
+#include <stdexcept>
+#include <string>
+#include <unistd.h>
 
 extern "C"
 {
@@ -17,6 +22,95 @@ const auto& FOREGROUND = tfb_white;
 const auto INTERVAL = 100ms;
 const auto POINTS = 5;
 const auto SPEED_FACTOR = 0.02f;
+
+
+class Input
+{
+public:
+    enum class Key
+    {
+        P1_UP,
+        P1_DOWN,
+        P2_UP,
+        P2_DOWN,
+    };
+
+    Input(const std::string& deviceFile)
+     : mDev(nullptr)
+    {
+        int fd = open(deviceFile.c_str(), O_RDONLY|O_NONBLOCK);
+        if (fd < 0) {
+                fprintf(stderr, "Could not open input device %s: %s\n", deviceFile.c_str(), strerror(-fd));
+                exit(1);
+        }
+        int rc = libevdev_new_from_fd(fd, &mDev);
+        if (rc < 0) {
+                fprintf(stderr, "Failed to init libevdev (%s)\n", strerror(-rc));
+                exit(1);
+        }
+
+        printf("Input device name: \"%s\"\n", libevdev_get_name(mDev));
+
+        if (!libevdev_has_event_type(mDev, EV_KEY)) {
+                printf("This device does not look like a keyboard\n");
+                exit(1);
+        }
+
+        std::queue<Key> unmappedKeys({Key::P1_UP, Key::P1_DOWN, Key::P2_UP, Key::P2_DOWN});
+        for (unsigned int code = 0; code < KEY_MAX; code++)
+        {
+            if (libevdev_has_event_code(mDev, EV_KEY, code))
+            {
+                const auto key = unmappedKeys.front();
+                printf("Using key code %s for key %d\n", libevdev_event_code_get_name(EV_KEY, code), (int)key);
+                mKeyMapping[code] = key;
+                unmappedKeys.pop();
+                if (unmappedKeys.empty())
+                    break;
+            }
+        }
+
+        if (not unmappedKeys.empty())
+        {
+            throw(std::runtime_error("Not enough events found"));
+        }
+    }
+
+    ~Input()
+    {
+        if (mDev != nullptr)
+        {
+            libevdev_free(mDev);
+            mDev = nullptr;
+        }
+    }
+
+    void process()
+    {
+        bool done = false;
+        while (not done) {
+            struct input_event ev;
+            int rc = libevdev_next_event(mDev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
+            if ((rc == 0) and (ev.type == EV_KEY) and mKeyMapping.find(ev.code) != mKeyMapping.end())
+            {
+                const auto key = mKeyMapping[ev.code];
+                mState[key] = ev.value != 0;
+                printf("Event: %d %d\n", (int)key, ev.value);
+            }
+            if (rc != LIBEVDEV_READ_STATUS_SYNC and rc !=  LIBEVDEV_READ_STATUS_SUCCESS)
+                done = true;
+        }
+    }
+
+    bool isKeyActive(Key key)
+    {
+        return mState[key];
+    }
+private:
+    libevdev *mDev;
+    std::map<Key, bool> mState;
+    std::map<unsigned int, Key> mKeyMapping;
+};
 
 
 class PongField
@@ -165,6 +259,7 @@ private:
     int mBallY;
 };
 
+
 class PongGame
 {
 public:
@@ -246,8 +341,18 @@ private:
 int main(int argc, char **argv)
 {
     char* fbdev = nullptr;
+    std::string evdev("/dev/input/event0");
     if (argc > 1)
         fbdev = argv[1];
+    if (argc > 2)
+        evdev = argv[2];
+
+    Input i(evdev);
+
+    while (true)
+    {
+        i.process();
+    }
 
     int rc;
     rc = tfb_acquire_fb(TFB_FL_NO_TTY_KD_GRAPHICS | TFB_FL_USE_DOUBLE_BUFFER, fbdev, nullptr);
